84ebd5cc9879c653b9d7690840f2114340f84ec8
eduardo-hdez <ehdez2004@gmail.com>
2026-03-07 19:01:47 -0600

    [Feat] add postLogin controller

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -2,6 +2,9 @@ exports.getLogin = (request, response, next) => {
   response.render("Login/index", { title: "Login" })
 };
 
-exports.postLogin = (request, response, next) => {};
+exports.postLogin = (request, response, next) => {
+  request.session.username = request.body.username;
+  response.redirect("/tasks");
+};
 
 exports.getLogout = (request, response, next) => {};
