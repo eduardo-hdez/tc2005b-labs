d13365b8e36cf2204ad8639e4a6660ed58c78839
eduardo-hdez <ehdez2004@gmail.com>
2026-03-10 12:52:59 -0600

    [Fix] correct controller names in routes

--- routes/tasks.routes.js
+++ routes/tasks.routes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 
 const tasksController = require("../controllers/tasks.controller");
 
-router.get("/", tasksController.getTasks);
+router.get("/", tasksController.getAllTasks);
 router.get("/new", tasksController.getNewTask);
 router.post("/new", tasksController.postNewTask);
 router.get("/:id/edit", tasksController.getEditTask);
 router.post("/:id/edit", tasksController.postEditTask);
-router.get("/:id", tasksController.getTasks);
+router.get("/:id", tasksController.getAllTasks);
 
 module.exports = router;
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const usersController = require("../controllers/users.controller");
 
+router.get("/signup", usersController.getSignup);
+router.post("/signup", usersController.postSignup);
 router.get("/login", usersController.getLogin);
 router.post("/login", usersController.postLogin);
 router.get("/logout", usersController.getLogout);
